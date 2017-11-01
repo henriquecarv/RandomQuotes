54f957d8f6142d6116f1d9f8d13685f4ece913ef
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2017-11-01 08:59:51 -0200

    adding copyrights

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@
             if (request.readyState == 4) {
                 if (request.status == 200) {
                     quoteText.innerHTML = request.response.quote;
-                    quoteAuthor.innerHTML = request.response.author + ' <cite title="Source"><a href="https://talaikis.com/random_quotes_api/">Talaikis Quotes</a></cite>';
+                    quoteAuthor.innerHTML = request.response.author + ' (©<cite title="Source"> <a href="https://talaikis.com/random_quotes_api/">Talaikis Quotes</a></cite>)';
                     var twitterText = request.response.quote + " - " + request.response.author;
                     createTwitterButton(twitterText);
                 } else
