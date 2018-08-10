3ccaf36a710b96576bc158103dc954fc4bb3c13d
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-08-10 14:25:31 -0300

    Update JS syntax

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,53 +1,51 @@
-(function ($) {
-    var createTwitterButton = function (text) {
-        var twitterShareButton = document.getElementById('twitter-share-button');
-        twttr.widgets.createShareButton(
-            "/",
-            twitterShareButton, {
-                size: "large",
-                text: text,
-                via: "henriquecarv",
-                related: "twitterapi,twitter"
-            }
-        );
-    }
+(function($) {
+	const createTwitterButton = text => {
+		const twitterShareButton = document.getElementById('twitter-share-button');
+		twttr.widgets.createShareButton('/', twitterShareButton, {
+			size: 'large',
+			text: text,
+			via: 'henriquecarv',
+			related: 'twitterapi,twitter'
+		});
+	};
 
-    var cardBody = document.getElementsByClassName('card-body')[0];
+	const cardBody = document.getElementsByClassName('card-body')[0];
 
-    var getQuote = function () {
-        cardBody.classList.add('fade-out');
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    var quoteText = document.getElementById('quote');
-                    var quoteAuthor = document.getElementById('author');
-                    quoteText.innerHTML = request.response.quote;
-                    quoteAuthor.innerHTML = request.response.author + ' (©<cite title="Source"> <a href="https://talaikis.com/random_quotes_api/">Talaikis Quotes</a></cite>)';
-                    var twitterText = request.response.quote + " - " + request.response.author;
-                    createTwitterButton(twitterText);
-                } else
-                    quoteText.innerHTML = "An error occurred during your request";
-            }
-        }
-        request.open('Get', 'https://talaikis.com/api/quotes/random/');
-        request.responseType = 'json';
-        request.send();
+	const getQuote = () => {
+		cardBody.classList.add('fade-out');
+		const request = new XMLHttpRequest();
+		request.onreadystatechange = () => {
+			if (request.readyState == 4) {
+				if (request.status == 200) {
+					const quoteText = document.getElementById('quote');
+					const quoteAuthor = document.getElementById('author');
+					quoteText.innerHTML = request.response.quote;
+					quoteAuthor.innerHTML =
+						request.response.author +
+						' (©<cite title="Source"> <a href="https://talaikis.com/random_quotes_api/">Talaikis Quotes</a></cite>)';
+					const twitterText =
+						request.response.quote + ' - ' + request.response.author;
+					createTwitterButton(twitterText);
+				} else quoteText.innerHTML = 'An error occurred during your request';
+			}
+		};
+		request.open('Get', 'https://talaikis.com/api/quotes/random/');
+		request.responseType = 'json';
+		request.send();
 
-        setTimeout(function () {
-            cardBody.classList.add('fade-in');
-            cardBody.classList.remove('fade-out');
-            cardBody.classList.remove('is-paused');
-        }, 500);
-    };
+		setTimeout(() => {
+			cardBody.classList.add('fade-in');
+			cardBody.classList.remove('fade-out');
+			cardBody.classList.remove('is-paused');
+		}, 500);
+	};
 
-    var newQuote = document.getElementById('newQuote');
-    newQuote.addEventListener('click', function (e) {
-        document.getElementsByClassName('twitter-share-button')[0].remove();
-        cardBody.classList.replace('fade-in', 'fade-out');
-        getQuote();
-    });
+	const newQuote = document.getElementById('newQuote');
+	newQuote.addEventListener('click', () => {
+		document.getElementsByClassName('twitter-share-button')[0].remove();
+		cardBody.classList.replace('fade-in', 'fade-out');
+		getQuote();
+	});
 
-    getQuote();
-
-}(jQuery));
+	getQuote();
+})(jQuery);
