24eefd8d43bc36aa471fb44177dad5cea278d44c
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-08-21 11:12:17 -0300

    Add OptimizeCss

--- webpack.config.js
+++ webpack.config.js
@@ -2,78 +2,87 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: ['./assets/js/main.js', './assets/sass/style.scss'],
-    output: {
-        filename: './dist/js/bundle.js',
-        path: path.join(__dirname, './')
-    },
-    devServer: {
-        contentBase: './build'
-    },
-    module: {
-        rules: [{
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true
-                    }
-                }]
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                            loader: 'css-loader',
-                            options: {
-                                minimize: true,
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader'
-                        },
-                        {
-                            loader: 'sass-loader'
-                        }
-                    ]
-                })
-            },
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './template/index.html',
-            filename: './index.html'
-        }),
-        new ExtractTextPlugin({
-            filename: './dist/css/style.css'
-        }),
-        new UglifyJSPlugin({
-            uglifyOptions: {
-                output: {
-                    comments: false
-                }
-            }
-        }),
-        new MinifyPlugin({}),
-        new webpack.ProvidePlugin({
-            $: 'jquery/dist/jquery.slim.js',
-            jQuery: 'jquery/dist/jquery.slim.js',
-            'window.jQuery': 'jquery/dist/jquery.slim.js',
-            Popper: 'popper.js/dist/umd/popper'
-        })
-    ]
-}
+	entry: ['./assets/js/main.js', './assets/sass/style.scss'],
+	output: {
+		filename: './dist/js/bundle.js',
+		path: path.join(__dirname, './'),
+	},
+	devServer: {
+		contentBase: './build',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								minimize: true,
+							},
+						},
+						{
+							loader: 'postcss-loader',
+						},
+						{
+							loader: 'sass-loader',
+						},
+					],
+				}),
+			},
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env'],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './template/index.html',
+			filename: './index.html',
+		}),
+		new ExtractTextPlugin({
+			filename: './dist/css/style.css',
+		}),
+		new OptimizeCssAssetsPlugin({
+			cssProcessorOptions: {discardComments: {removeAll: true}},
+			canPrint: true,
+		}),
+		new UglifyJSPlugin({
+			uglifyOptions: {
+				output: {
+					comments: false,
+				},
+			},
+		}),
+		new MinifyPlugin({}),
+		new webpack.ProvidePlugin({
+			$: 'jquery/dist/jquery.slim.js',
+			jQuery: 'jquery/dist/jquery.slim.js',
+			'window.jQuery': 'jquery/dist/jquery.slim.js',
+			Popper: 'popper.js/dist/umd/popper',
+		}),
+	],
+};
