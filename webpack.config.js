7a219942bb1168f11988302b0569fd6306620fce
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-12-16 13:13:44 -0200

    Update packages and small fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,77 +6,77 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-	entry: ['./assets/js/main.js', './assets/sass/style.scss'],
-	output: {
-		filename: './dist/js/bundle.js',
-		path: path.resolve(__dirname, './'),
-	},
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: true, // set to true if you want JS source maps
-				uglifyOptions: {
-					output: {
-						comments: false,
-					},
-				},
-			}),
-			new OptimizeCSSAssetsPlugin({
-				cssProcessorOptions: {discardComments: {removeAll: true}},
-				canPrint: true,
-			}),
-		],
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: './template/index.html',
-			filename: './index.html',
-		}),
-		new MiniCssExtractPlugin({
-			// Options similar to the same options in webpackOptions.output
-			// both options are optional
-			filename: './dist/css/style.css',
-		}),
-		new webpack.ProvidePlugin({
-			$: 'jquery/dist/jquery.slim.js',
-			jQuery: 'jquery/dist/jquery.slim.js',
-			'window.jQuery': 'jquery/dist/jquery.slim.js',
-			Popper: 'popper.js/dist/umd/popper',
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
-						options: {
-							minimize: true,
-						},
-					},
-				],
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{loader: MiniCssExtractPlugin.loader},
-					{loader: 'css-loader', options: {minimize: true}},
-					{loader: 'postcss-loader'},
-					{loader: 'sass-loader'},
-				],
-			},
-			{
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-		],
-	},
+  entry: ['./assets/js/main.js', './assets/sass/style.scss'],
+  output: {
+    filename: './dist/js/bundle.js',
+    path: path.resolve(__dirname, './'),
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // set to true if you want JS source maps
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: { discardComments: { removeAll: true } },
+        canPrint: true,
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './template/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: './dist/css/style.css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery/dist/jquery.slim.js',
+      jQuery: 'jquery/dist/jquery.slim.js',
+      'window.jQuery': 'jquery/dist/jquery.slim.js',
+      Popper: 'popper.js/dist/umd/popper',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
