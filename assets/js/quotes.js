cdd9aad6532c4f7368eccaddba100bc3ecba42ed
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2019-03-09 15:53:07 -0300

    Update with fetch() API

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,59 +1,63 @@
-(function($) {
-  const twitterShareButton = document.getElementById('twitter-share-button');
+;(function ($) {
+  const twitterShareButton = document.getElementById('twitter-share-button')
 
-  const createTwitterButton = (text) => {
+  const createTwitterButton = text => {
     twttr.ready(() => {
       twttr.widgets.createShareButton('/', twitterShareButton, {
         size: 'large',
         text: text,
         via: 'henriquecarv',
-        related: 'twitterapi,twitter',
-      });
-    });
-  };
+        related: 'twitterapi,twitter'
+      })
+    })
+  }
 
-  const cardBody = document.getElementsByClassName('card-body')[0];
+  const cardBody = document.getElementsByClassName('card-body')[0]
 
-  const getQuote = () => {
-    cardBody.classList.add('fade-out');
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-      if (request.readyState === 4) {
-        const quoteText = document.getElementById('quote');
-        if (request.status === 200) {
-          const quoteAuthor = document.getElementById('author');
-          quoteText.innerHTML = request.response[0].quote;
-          quoteAuthor.innerHTML =
-            request.response[0].author +
-            ' (©<cite title="Source"> <a href="https://market.mashape.com/andruxnet/random-famous-quotes">Random Famous Quotes</a></cite>)';
-          const twitterText =
-            request.response[0].quote + ' - ' + request.response[0].author;
-          createTwitterButton(twitterText);
-        } else quoteText.innerHTML = 'An error occurred during your request';
-      }
-    };
-    request.open(
-      'Get',
+  const getQuote = async () => {
+    cardBody.classList.add('fade-out')
+
+    const headers = new Headers({
+      'X-Mashape-Key': process.env.API_KEY,
+      Accept: 'application/json'
+    })
+    const request = new Request(
       'https://andruxnet-random-famous-quotes.p.mashape.com/',
-    );
-    request.setRequestHeader('X-Mashape-Key', process.env.API_KEY);
-    request.setRequestHeader('Accept', 'application/json');
-    request.responseType = 'json';
-    request.send();
+      { method: 'GET', headers, mode: 'cors', cache: 'no-cache' }
+    )
 
-    setTimeout(() => {
-      cardBody.classList.add('fade-in');
-      cardBody.classList.remove('fade-out');
-      cardBody.classList.remove('is-paused');
-    }, 500);
-  };
+    const quoteText = document.getElementById('quote')
 
-  const newQuote = document.getElementById('newQuote');
+    try {
+      const response = await fetch(request)
+      const [result] = await response.json()
+
+      const { quote, author } = result
+
+      const quoteAuthor = document.getElementById('author')
+      quoteText.innerHTML = quote
+      quoteAuthor.innerHTML = `${author} (©<cite title="Source"> <a href="https://market.mashape.com/andruxnet/random-famous-quotes">Random Famous Quotes</a></cite>)`
+
+      const twitterText = `${quote} - ${author}`
+      createTwitterButton(twitterText)
+
+      setTimeout(() => {
+        cardBody.classList.add('fade-in')
+        cardBody.classList.remove('fade-out')
+        cardBody.classList.remove('is-paused')
+      }, 500)
+    } catch (error) {
+      console.error(error.message)
+      quoteText.innerHTML = 'An error occurred during your request'
+    }
+  }
+
+  const newQuote = document.getElementById('newQuote')
   newQuote.addEventListener('click', () => {
-    twitterShareButton.remove();
-    cardBody.classList.replace('fade-in', 'fade-out');
-    getQuote();
-  });
+    twitterShareButton.remove()
+    cardBody.classList.replace('fade-in', 'fade-out')
+    getQuote()
+  })
 
-  getQuote();
-})(jQuery);
+  getQuote()
+})(jQuery)
